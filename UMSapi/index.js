8cb854625241fc7102be9850338d3a4972bcf4cc
farzanashaneez <farzanashaneez@gmail.com>
2024-09-23 23:09:43 +0400

    connect to database

--- UMSapi/index.js
+++ UMSapi/index.js
@@ -1,7 +1,16 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app=express();
+mongoose
+  .connect(process.env.MONGO_CONNECT)
+  .then(() => {console.log('connected to mongodb')})
+  .catch((err) => {
+    console.log(err)
+  });
 
-app.listen(3000,()=>{
-    console.log("server listening on port 3000")
-})
+const app = express();
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
